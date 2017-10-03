00d7239d15f57c7d87f90fc29e1c2bd5af6b533f
yashwanthkoneri <yaswanthkoneri@gmail.com>
2017-10-03 19:35:25 +0530

    created admin-page and added html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AdminPageComponent } from './admin-page/admin-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminPageComponent
   ],
   imports: [
     BrowserModule
