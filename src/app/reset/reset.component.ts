95fe79fd4d41ef1eff82304c48709a50fb7f17a5
yashwanthkoneri <yaswanthkoneri@gmail.com>
2017-10-24 17:13:19 +0530

    reset component

---
+++ src/app/reset/reset.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute,Router } from '@angular/router';
+import { AuthService } from '../auth.service';
+
+@Component({
+  selector: 'app-reset',
+  templateUrl: './reset.component.html',
+  styleUrls: ['./reset.component.css']
+})
+export class ResetComponent implements OnInit {
+
+  constructor(private authenticationService : AuthService,
+    private router : Router,
+    private route: ActivatedRoute) { }
+
+  ngOnInit() {
+  }
+  email:any;
+  otp:any;
+  newpassword:any;
+  reset(){
+    this.authenticationService.resetPassword(this.email,this.otp,this.newpassword ).subscribe(res =>{
+      if(res){
+      console.log("Password Reset successfully")
+                  this.router.navigate(['/login']);
+        }
+    })
+  }
+}
