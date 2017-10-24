37ce7bb3d1a2a6ef40550603ded350ca49e614ef
yashwanthkoneri <yaswanthkoneri@gmail.com>
2017-10-24 17:15:04 +0530

    signup component updated

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute,Router } from '@angular/router';
+import { AuthService } from '../auth.service';
+
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +10,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authenticationService : AuthService,
+    private router : Router,
+    private route: ActivatedRoute) { }
 
   ngOnInit() {
   }
+  email:any;
+  phone:any;
+  password:any;
+  confirmpassword: any;
+  referral:any;
+ 
 
+  _isFormValid(){
+    if(!this.email || this.email == ""){
+        return false;
+    }
+    if(!this.phone || this.phone == ""){
+    		return false;
+    } 
+      	if(!this.password || this.password == ""){
+    		return false;
+    }
+    if(!this.confirmpassword || this.confirmpassword == ""){
+        return false;
+    } 
+    return true;
+}
+ValidateEmail()  
+{  
+  var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; 
+  return this.email.match(mailformat) ? true : false; 
+}
+signUp(){
+  if(!this._isFormValid()){
+    alert("Please fill required fields");
+    return;
+}
+  this.authenticationService.signUp(this.email,this.phone,this.password,this.confirmpassword,this.referral).subscribe(res =>{
+    
+    if(res){
+      console.log("signup  succesfull");
+              this.router.navigate(['/login']);
+    }
+  }) 
+}
 }
