d215b70082156e615a309c89787d7a46152813b5
yashwanthkoneri <yaswanthkoneri@gmail.com>
2017-10-24 17:14:35 +0530

    login component updated

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute,Router } from '@angular/router';
+import { AuthService } from '../auth.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  constructor(private authenticationService : AuthService,
+    private router : Router,
+    private route: ActivatedRoute) { }
+
+  ngOnInit() {
+  }
+  email:any;
+  password:any;
+  login(){
+    this.authenticationService.login(this.email,this.password).subscribe(res =>{
+      if(res){
+        console.log("login  succesfull");
+      }    
+    })
+  }
+}
