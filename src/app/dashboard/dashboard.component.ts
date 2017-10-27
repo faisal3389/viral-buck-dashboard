3d436b0aa6522f88046673591b2ce2e9ca8cb0f7
Yaswanth koneri <habeeb@sellergyan.com>
2017-10-27 16:06:43 +0530

    dashboard component working ts  file

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import {DashboardService} from '../dashboard.service'
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'],
+  providers: [DashboardService]
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private DashboardService : DashboardService,
+  ) { }
 
   ngOnInit() {
+    this.dashboard()
   }
 
+  dashboard(){
+  let email = localStorage.getItem('email');
+  this.DashboardService.dashboard(email).subscribe(res =>{
+    console.log(res)
+  
+  })
+  }
 }
