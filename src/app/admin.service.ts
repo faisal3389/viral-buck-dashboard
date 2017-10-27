002f4615d02c7ea118499fb97bf751e5f93618ad
Yaswanth koneri <habeeb@sellergyan.com>
2017-10-27 16:05:23 +0530

    admin service added

--- src/app/admin.service.ts
+++ src/app/admin.service.ts
@@ -1,13 +1,8 @@
 import { Injectable } from '@angular/core';
 import {baseUrl} from './constants';
-import {Observable} from 'rxjs/Rx';
-import {
-    Jsonp,
-    Http,
-    Response,
-    Headers,
-    RequestOptions
-} from '@angular/http';
+import 'rxjs/add/operator/map';
+import { Observable } from 'rxjs/Rx';
+import { Jsonp, Http, Response, Headers, RequestOptions } from '@angular/http';
 
 @Injectable()
 export class AdminService {
@@ -17,39 +12,19 @@ export class AdminService {
     private handleError(error:any){
         return Observable.throw(error.json().error||'Server error');
     }
-    PostTask(url : String){
-        let ipurl = this.BASE_URL+'short';
+    PostTask(url : String,assigned_to:String){
+        let ipurl = this.BASE_URL+'admin/'+'task';
         
         let body  = JSON.stringify(url);
+        let dataString = "url="+body+"&"+"assigned_to="+assigned_to;
+        
         let headers = new Headers({ 'Content-type': 'application/json' });
         let options = new RequestOptions({ headers: headers });
     
-        return this.http.post(ipurl, body,options)
+        return this.http.post(ipurl, dataString,options)
           .map((res:Response) => res.json())
           .catch(this.handleError);
       }
-    // PostTask(url: String){
-    //     // let url =  this.nodeurl+'/products/new';
-    //     let ipurl = this.BASE_URL+'short';
-    //     let body = JSON.stringify( url );
-    //     console.log(body,ipurl)
-    //     let headers = new Headers({ 'Content-Type': 'application/json' });
-    //     let options = new RequestOptions({ headers: headers });
-    //     return this.http.post(ipurl,body,options)
-    //         .map((res:Response) => res.json()) 
-    //         .catch(this.handleError); 
-    //   }
+ 
 }
 
-// PostTask (url:string){
-//     let ipurl = this.BASE_URL+'/posturl';
-//     return this.http.get(ipurl)
-//         .map((res:Response) => res.json()) 
-//         .catch((error:any) => Observable.throw(error || 'Server error'));
-//   }
-//     getProductFromAsinForProductPage(asin:string){
-//     let url =this.nodeurl+'/productasin/'+asin;
-//     return this.http.get(url) 
-//         .map((res:Response) => res.json()) 
-//         .catch((error:any) => Observable.throw(error || 'Server error'));
-//   } 
