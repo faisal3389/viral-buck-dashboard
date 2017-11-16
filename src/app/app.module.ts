2ad3c723c29112cda7870cfb086596452dc254c0
Yaswanth Koneri <knyaswanth@iitrpr.ac.in>
2017-11-16 13:08:46 +0530

    Temp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {mainRouterModule} from './app.route'
-import {AdminService} from './admin.service'
-
+import {mainRouterModule} from './app.route';
+import {AdminService} from './admin.service';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { AdminPageComponent } from './admin-page/admin-page.component';
 
@@ -14,9 +15,11 @@ import { AdminPageComponent } from './admin-page/admin-page.component';
   imports: [
     BrowserModule,
     mainRouterModule,
-    AdminService
+    FormsModule,
+    HttpModule
+    
   ],
-  providers: [],
+    providers: [AdminService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
