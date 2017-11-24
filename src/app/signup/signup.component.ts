31dbea064b29dace1ed29af68ee636e335bcea4a
Giri Tamada <Naresh@Naresh.Naresh>
2017-11-24 12:42:46 +0530

    removed unwanted code

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute,Router } from '@angular/router';
 import { AuthService } from '../auth.service';
-// import { NotificationService } from '../models/notification.service';
 
 
 @Component({
@@ -15,7 +14,6 @@ export class SignupComponent implements OnInit {
 
   constructor(private authenticationService : AuthService,
     private router : Router,
-    // private _notification: NotificationService,
     private route: ActivatedRoute){ }
 
   ngOnInit() {
@@ -39,45 +37,47 @@ export class SignupComponent implements OnInit {
     return false;
   }
   if(!this.confirmPassword || this.confirmPassword == ""){
-    
       return false;
   }
   
    return true;
 }
+  /**
+   * Validating the email
+   */
   ValidateEmail(){  
     var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; 
     return this.email.match(mailformat) ? true : false; 
   }
+  /**
+   * Signup will be starting fro here
+   */
   signUp(){
     if(!this._isFormValid()){
       alert("Please Fill Required Fields");
       return;
     }
-      this.authenticationService.signUp(this.email,this.phoneNumber,this.password,this.confirmPassword).subscribe(res =>{
-        console.log("response",res);
-        if(this.ValidateEmail() && res.status=='success'){
-          console.log("signup  succesfull");
-        alert("user regisration successfull")
-          // this._notification.notify("User registration successful");
-          this.router.navigate(['/login']);
-        }
-        if(this.ValidateEmail()==false){
+    this.authenticationService.signUp(this.email,this.phoneNumber,this.password,this.confirmPassword).subscribe(res =>{
+      if(this.ValidateEmail() && res.status=='success'){
+       alert("user regisration successfull")
+       this.router.navigate(['/login']);
+      }
+      if(this.ValidateEmail()==false){
         alert("Enter a valid Email Id");
-        }
-        if(this.ValidateEmail()==true && res && res.statusCode == "401"){
+      }
+      if(this.ValidateEmail()==true && res && res.statusCode == "401"){
         alert("User already registered");
-        }
-        else if(this.password!== this.confirmPassword){
-          alert("Password and Confirm Password Should be Same");
-       } 
-        else if(res.status !== "success"){
-          alert("Signup failed, Please try again");
-        }
-        else if(!res){
-          console.log("API Error");
-        }
-      })
+      }
+      else if(this.password!== this.confirmPassword){
+        alert("Password and Confirm Password Should be Same");
+     } 
+     else if(res.status !== "success"){
+        alert("Signup failed, Please try again");
+     }
+     else if(!res){
+        console.log("API Error");
+      }
+    })
     
   }
 }
